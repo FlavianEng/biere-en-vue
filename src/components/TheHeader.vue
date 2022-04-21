9c1f32cf3809b976527e65815fa8e4a19e6ec4bf
Flavian Engevin <flavian.engevin@ynov.com>
2022-04-21 11:33:01 +0200

    Add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="flex h-full justify-center items-center bg-yellow">
+    <div class="py-10">
+      <RouterLink to="/"> <img :src="logo" class="h-14 w-full" /> </RouterLink>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import logo from '@/assets/logo.svg';
+
+export default defineComponent({
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: logo,
+    };
+  },
+});
+</script>
