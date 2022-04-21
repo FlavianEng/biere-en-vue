b3a5e99a6e699fd02a58fb476d7224b9b83357f0
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-04-21 15:38:04 +0200

    🚀 Add InputContainer component

---
+++ src/components/Fields/InputContainer.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    className: {
+      default: '',
+      type: String,
+    },
+    errorMessage: {
+      default: '',
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    htmlFor(): string {
+      return `form-${this.name}`;
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col" :class="className">
+    <label v-if="label" :for="htmlFor" class="mb-2 font-bold">{{ label }}</label>
+    <slot />
+    <span v-if="errorMessage" class="text-red w-full mt-1">{{ errorMessage }}</span>
+  </div>
+</template>
