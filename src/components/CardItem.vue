3518c416010a82f791fe46b52ea4854f210fa687
Flavian Engevin <flavian.engevin@ynov.com>
2022-04-21 14:30:25 +0200

    Fix reviews

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -5,17 +5,31 @@ import exampleBottle from '@/assets/images/defaultBeer.png';
 export default defineComponent({
   name: 'CardItem',
   props: {
-    description: String,
-    imgUrl: String,
-    subtitle: String,
-    title: String,
+    description: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
   },
   data() {
     return {
-      exampleBottle: exampleBottle,
+      exampleBottle,
     };
   },
   computed: {
+    getAlt(): string {
+      return 'Visuel de la bière'.concat(' ', this.title);
+    },
     getImgUrl(): string {
       let beerImgUrl = this.imgUrl;
 
@@ -26,21 +40,19 @@ export default defineComponent({
       return beerImgUrl as string;
     },
   },
-  methods: {},
 });
 </script>
 
 <template>
   <div class="py-5 px-8 bg-grey-light rounded-xl flex">
-    <img :src="getImgUrl" alt="Visuel de la bière" class="h-64 text-vertical mr-10" />
+    <img :src="getImgUrl" :alt="getAlt" class="h-64 text-vertical mr-10" />
     <div>
-      <p class="text-title">Trashy blonde</p>
-      <p class="text-subtitle mb-5">You know you shouldn't</p>
+      <p class="text-title">{{ title }}</p>
+      <p class="text-subtitle mb-5">{{ subtitle }}</p>
       <p class="mb-8 h-[5.800rem] line-clamp-3">
-        A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance,
-        and a little bit of low self esteem
+        {{ description }}
       </p>
-      <button>Découvrir</button>
+      <button>Discover</button>
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,28 @@
 <script lang="ts">
-import CardItem from '../components/CardItem.vue';
+import CardItem from '@/components/CardItem.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   components: { CardItem },
-  data() {
-    return {};
-  },
 });
 </script>
 
 <template>
   <div class="grid gap-5 grid-cols-1 md:grid-cols-2">
-    <CardItem imgUrl="" />
-    <CardItem imgUrl="" />
-    <CardItem imgUrl="" />
+    <CardItem
+      imgUrl="d"
+      title="Wheit a minute"
+      subtitle="You know you shouldn't"
+      description="This is the description of the beer" />
+    <CardItem
+      imgUrl=""
+      title="Wheit a minute"
+      subtitle="You know you shouldn't"
+      description="This is the description of the beer" />
+    <CardItem
+      imgUrl=""
+      title="Wheit a minute"
+      subtitle="You know you shouldn't"
+      description="This is the description of the beer" />
   </div>
 </template>
