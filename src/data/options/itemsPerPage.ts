f636221796ed1d4c9cf15fbc1c107efd626cce84
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-04-28 14:25:55 +0200

    🚀 Fix

--- src/data/options/itemsPerPage.ts
+++ src/data/options/itemsPerPage.ts
@@ -3,19 +3,19 @@ import type { Option } from '@/components/Fields/SelectField.vue';
 const optionsItemsPerPage: Option[] = [
   {
     name: 6,
-    value: '6 par pages',
+    value: '6 per pages',
   },
   {
     name: 12,
-    value: '12 par pages',
+    value: '12 per pages',
   },
   {
     name: 18,
-    value: '18 par pages',
+    value: '18 per pages',
   },
   {
     name: 24,
-    value: '24 par pages',
+    value: '24 per pages',
   },
 ];
 
